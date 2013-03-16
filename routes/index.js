0663ee76be16c1d3a216da74da109b373eb71d8c
Takashi Miwa <sanrinsha.tm@gmail.com>
2013-03-16 23:17:22 +0900

    MongoDBからユーザーIDを取ってくる

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,26 @@
  * GET home page.
  */
 
+var model = require('../model/model');
+var User = model.User;
+
 exports.index = function(req, res){
-  res.render('index', { title: 'じだらくスケジューラ' });
+    res.render('index', { title: 'じだらくスケジューラ' });
 };
+
+exports.form = function(req, res){
+  res.render('form', { title: 'New Entry' })
+};
+
+exports.create = function(req, res){
+  var newPost = new Post(req.body);
+  newPost.save(function(err){
+     if (err) {
+       console.log(err);
+       res.redirect('back');
+     } else {
+       res.redirect('/');
+     }
+    });
+};
+
