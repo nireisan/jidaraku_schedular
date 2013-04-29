ce5cdf2b7632c2c53e5b2b019dd1e733e8eacea9
Takashi Miwa <sanrinsha.tm@gmail.com>
2013-04-29 15:54:00 +0900

    createItem

--- libs/sampleSocketServer.js
+++ libs/sampleSocketServer.js
@@ -6,11 +6,13 @@ io.of( '/sample' ).on( 'connection', function ( socket ) {
 
     console.log( 'connect sample!!!' );
 
+    /*
     socket.on( 'reqEventList', function( userId ) {
         database.getEventList(userId, null, function(eventList) {
             socket.emit( 'resEventList', eventList);
         });
     });
+    */
 
     /**
      * data = {
@@ -22,15 +24,33 @@ io.of( '/sample' ).on( 'connection', function ( socket ) {
      *      }
      * }
      */
+    /*
     socket.on( 'reqCreateEvent', function( data ) {
         database.createEvent(data, function(eventInfo) {
             socket.emit( 'resCreateEvent', eventInfo);
         });
     });
+    */
 
-    socket.on( 'reqDeleteEvent', function( userId ) {
-        database.getEventList(userId, null, function(eventList) {
-            socket.emit( 'resDeleteEvent', eventList);
+    // socket.on( 'reqDeleteEvent', function( userId ) {
+    //     database.getEventList(userId, null, function(eventList) {
+    //         socket.emit( 'resDeleteEvent', eventList);
+    //     });
+    // });
+
+   /*
+        {
+            EventId: 5,
+            UserId: "hkitamur",
+            ItemName: "new item だにぃ",
+            Comment: "新しく作ったよー！",
+            StartTime :  1234567890, // タイムスタンプ(ミリ秒)
+            EndTIme : 1234567890 // タイムスタンプ(ミリ秒)
+        }
+    */
+    socket.on( 'reqCreateItem', function( data ) {
+        database.createItem(data, function(itemList) {
+            socket.emit( 'resCreateItem', itemList);
         });
     });
 });
